42b1b7e08a8622221b6a0af15f2a1ad74b3a91e5
Yves Talboom <yves1@MacBook-Pro-15inch.local>
2016-06-06 17:15:10 +0200

    initial commit

--- app-readme.md
+++ app-readme.md
@@ -1,35 +1,6 @@
-# Framework Name • [TodoMVC](http://todomvc.com)
-
-> Official description of the framework (from its website)
-
-
-## Resources
-
-- [Website]()
-- [Documentation]()
-- [Used by]()
-- [Blog]()
-- [FAQ]()
-
-### Articles
-
-- [Interesting article]()
-
-### Support
-
-- [Stack Overflow](http://stackoverflow.com/questions/tagged/__)
-- [Google Groups]()
-- [Twitter](http://twitter.com/__)
-- [Google+]()
-
-*Let us [know](https://github.com/tastejs/todomvc/issues) if you discover anything worth sharing.*
-
+# CamJs • [TodoMVC](http://todomvc.com)
 
 ## Implementation
 
-How was the app created? Anything worth sharing about the process of creating the app? Any spec violations?
+TodoMVC implementation with CanJs
 
-
-## Credit
-
-Created by [Your Name](http://your-website.com)
--- js/app.js
+++ js/app.js
@@ -1,6 +1,150 @@
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+	can.fixture({
+		"GET /services/todos": function () {
+			return [
+				{name: 'Walk the dog', completed: true, id: 1},
+				{name: 'Mow the lawn', completed: false, id: 2},
+				{name: 'Learn canjs', completed: false, id: 3}
+			];
+		},
+		"DELETE /services/todos/{id}": function () {
+			return {};
+
+		},
+		"POST /services/todos": function () {
+			console.log('you just created a todo');
+			return {id: Math.random()}
+		},
+		"PUT /services/todos/{id}": function () {
+			console.log('you updated a todo on the server!');
+			return {};
+		}
+	});
+
+	var Todo = can.Model.extend({
+		findAll: "GET /services/todos",
+		destroy: "DELETE /services/todos/{id}",
+		create: "POST /services/todos",
+		update: "PUT /services/todos/{id}"
+	}, {});
+
+	window.Todo = Todo;
+
+	Todo.List = Todo.List.extend({
+		filter: function (check) {
+			var list = new this.constructor;
+			this.each(function (todo) {
+				if (check(todo)) {
+					list.push(todo);
+				}
+
+			});
+			return list
+		},
+		active: function () {
+			return this.filter(function (todo) {
+				return !todo.attr('completed');
+			})
+		},
+		completed: function () {
+			return this.filter(function (todo) {
+				return todo.attr('completed');
+			})
+		},
+		activeCount: function () {
+			return this.active().attr('length');
+		},
+		completedCount: function () {
+			return this.completed().attr('length');
+		}
+	});
+
+	can.Component.extend({
+		tag: "todos-create",
+		template: "<input class='new-todo' id='new-todo' placeholder='what needs to be done?' autofocus='' " +
+		"can-enter='createTodo'>",
+		scope: {
+			createTodo: function (context, el, ev) {
+				if (el.val()) {
+					new Todo({
+						completed: false,
+						name: el.val()
+					}).save();
+					el.val('');
+				}
+			}
+		}
+	});
+
+	can.Component.extend({
+		tag: 'todos-list',
+		template: can.view({url: 'list-template.html', engine: 'mustache'}),
+		scope: {
+			"editTodo": function (todo) {
+				todo.attr("editing", true);
+			},
+			"updateTodo": function (todo, el) {
+				todo.removeAttr('editing');
+				todo.attr('name', el.val());
+				todo.save();
+			}
+		}
+	});
+
+	can.Component.extend({
+		tag: 'todos-app',
+		scope: {
+			todos: new Todo.List({}),
+			displayedTodos: function () {
+				var filter = can.route.attr('filter');
+				var todos = this.attr('todos');
+
+				if (filter === 'active') {
+					return todos.active();
+				} else if (filter === 'completed') {
+					return todos.completed();
+				}
+
+				return todos;
+			}
+		},
+		helpers: {
+			filterLink: function (text, filterValue) {
+				var attrs = {};
+				if (filterValue) {
+					attrs.filter = filterValue;
+				}
+				return can.route.link(text, attrs, {
+					className: can.route.attr('filter') == filterValue ? "selected" : ""
+				});
+			},
+			plural: function (singular, count) {
+				var value = count()();
+				if (value == 1) {
+					return singular;
+				} else {
+					return singular + 's';
+				}
+			}
+		},
+		events: {
+			"{Todo} created": function (Todo, ev, newTodo) {
+				console.log('event created');
+				this.scope.attr('todos').push(newTodo);
+			}
+		}
+	});
+
+	can.route(':filter');
+	can.route.ready();
+
+	var frag = can.view({
+		url: "app-template.html", engine: "mustache"
+	}, {});
+
+	$('#app').html(frag);
+
 
 })(window);
